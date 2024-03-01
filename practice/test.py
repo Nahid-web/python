b86c642b33fdec62f23c41a36d31b5cf0763c13d
Nahid-web <nahidamin266@gmail.com>
2024-03-02 00:15:40 +0600

    operator , bolian, data type

---
+++ practice/test.py
@@ -0,0 +1,18 @@
+# print("Nahid Amin")
+
+# print(9>10) #false
+
+
+# all print flase
+""""
+print(bool(False))
+print(bool(0))
+print(bool(None))
+print(bool(''))
+print(bool({}))
+print(bool([]))
+print(bool())
+
+"""
+
+# print(10 + 5)
